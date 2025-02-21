160e10d20ea6dd5296af1337c3a188a148ca8703
Ronak Baldha <5601641+ron4ex@users.noreply.github.com>
2025-02-21 15:27:40 +0530

    Refactor CourseCover styles for responsiveness

--- packages/theme/src/Themed.tsx
+++ packages/theme/src/Themed.tsx
@@ -5,38 +5,40 @@ import { ThemeType } from "./types";
 
 type Element<T extends React.ElementType = "div"> = React.ElementRef<T>;
 
-interface PropsType extends ComponentPropsWithoutRef<"div"> {
+export interface ThemedProps extends ComponentPropsWithoutRef<"div"> {
   asChild?: boolean;
   as?: keyof JSX.IntrinsicElements;
   theme?: ThemeType;
   isInverted?: boolean;
 }
 
-export const Themed = forwardRef<Element, PropsType>((props, forwardedRef) => {
-  const { theme: nearestTheme } = useTheme();
+export const Themed = forwardRef<Element, ThemedProps>(
+  (props, forwardedRef) => {
+    const { theme: nearestTheme } = useTheme();
 
-  const {
-    children,
-    asChild,
-    isInverted,
-    theme,
-    as: Tag = "div",
-    ...rest
-  } = props;
+    const {
+      children,
+      asChild,
+      isInverted,
+      theme,
+      as: Tag = "div",
+      ...rest
+    } = props;
 
-  let resolvedTheme = theme || nearestTheme;
+    let resolvedTheme = theme || nearestTheme;
 
-  if (isInverted) {
-    resolvedTheme = nearestTheme === "light" ? "dark" : "light";
+    if (isInverted) {
+      resolvedTheme = nearestTheme === "light" ? "dark" : "light";
+    }
+
+    return (
+      <ThemeProvider theme={resolvedTheme}>
+        <Slot.Root {...rest} ref={forwardedRef} data-theme={resolvedTheme}>
+          {asChild ? children : <Tag>{children}</Tag>}
+        </Slot.Root>
+      </ThemeProvider>
+    );
   }
-
-  return (
-    <ThemeProvider theme={resolvedTheme}>
-      <Slot.Root {...rest} ref={forwardedRef} data-theme={resolvedTheme}>
-        {asChild ? children : <Tag>{children}</Tag>}
-      </Slot.Root>
-    </ThemeProvider>
-  );
-});
+);
 
 Themed.displayName = "Themed";
