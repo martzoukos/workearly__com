916dc38a2f239ba0a5a9cf336a83e0fa3403b2fe
Hamza Wattari <hamza.ayment@gmail.com>
2025-02-27 22:52:26 +0300

    adjustment to menu

--- apps/web/components/Menu/Menu.tsx
+++ apps/web/components/Menu/Menu.tsx
@@ -4,6 +4,7 @@ import Text from "@/components/Text";
 import Button from "@/components/Button";
 import { ChevronDown, ChevronRight, UserFilled } from "@carbon/icons-react";
 import MenuCertificateCard from "@/components/_cards/MenuCertificateCard";
+import MenuCourseCard from "@/components/_cards/MenuCourseCard";
 
 export default function Menu() {
   const [subMenu, setSubMenu] = useState(false);
@@ -25,109 +26,94 @@ export default function Menu() {
           </Button>
 
           {showMenu && (
-            <nav className={styles.navigation}>
-              <div className={styles.menuMainItem}>
-                <div className={styles.mainMenu}>
-                  <Text size="h5" className={styles.menuTitle}>
-                    By Goal
-                  </Text>
-                  <div
-                    className={styles.menuItem}
-                    onMouseEnter={() => setSubMenu(true)}
-                    onMouseLeave={() => setSubMenu(false)}
-                  >
-                    <Text size="small">HR</Text>
-                    <ChevronRight />
-                  </div>
-                </div>
-
-                {subMenu && (
-                  <div
-                    className={styles.submenu}
-                    onMouseEnter={() => setSubMenu(true)}
-                    onMouseLeave={() => setSubMenu(false)}
-                  >
+            <nav
+              className={styles.navigation}
+              onMouseLeave={() => setShowMenu((prev) => !prev)}
+            >
+              <div>
+                <div className={styles.menuMainItem}>
+                  <div className={styles.mainMenu}>
                     <Text size="h5" className={styles.menuTitle}>
                       By Goal
                     </Text>
-                    <div className={styles.menuItem}>
+                    <div
+                      className={styles.menuItem}
+                      onMouseEnter={() => setSubMenu(true)}
+                      onMouseLeave={() => setSubMenu(false)}
+                    >
                       <Text size="small">HR</Text>
                       <ChevronRight />
                     </div>
                   </div>
-                )}
+                </div>
+                <div className={styles.menuMainItem}>
+                  <div className={styles.mainMenu}>
+                    <div
+                      className={styles.menuItem}
+                      onMouseEnter={() => setSubMenuAdvanced(true)}
+                      onMouseLeave={() => setSubMenuAdvanced(false)}
+                    >
+                      <Text size="small">Data Science</Text>
+                      <ChevronRight />
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className={styles.menuMainItem}>
-                <div className={styles.mainMenu}>
-                  <div
-                    className={styles.menuItem}
-                    onMouseEnter={() => setSubMenuAdvanced(true)}
-                    onMouseLeave={() => setSubMenuAdvanced(false)}
-                  >
-                    <Text size="small">Data Science</Text>
+
+              {subMenu && (
+                <div
+                  className={styles.submenu}
+                  onMouseEnter={() => setSubMenu(true)}
+                  onMouseLeave={() => setSubMenu(false)}
+                >
+                  <Text size="h5" className={styles.menuTitle}>
+                    By Goal
+                  </Text>
+                  <div className={styles.menuItem}>
+                    <Text size="small">HR</Text>
                     <ChevronRight />
                   </div>
                 </div>
+              )}
 
-                {true && (
-                  <div
-                    className={styles.submenuAdanced}
-                    onMouseEnter={() => setSubMenuAdvanced(true)}
-                    onMouseLeave={() => setSubMenuAdvanced(false)}
-                  >
-                    <div className={styles.menuHeader}>
-                      <Text size="h5" className={styles.menuTitle}>
-                        Data Science
-                      </Text>
-                      <Text size="small" className={styles.menuDescription}>
-                        Data Science is an interdisciplinary field which
-                        utilizes scientific methods, algorithms, and data
-                        insights to extract meaning and drive informed
-                        decision-making across industries.
-                      </Text>
+              {subMenuAdvanced && (
+                <div
+                  className={styles.submenuAdanced}
+                  onMouseEnter={() => setSubMenuAdvanced(true)}
+                  onMouseLeave={() => setSubMenuAdvanced(false)}
+                >
+                  <div className={styles.menuHeader}>
+                    <Text size="h5" className={styles.menuTitle}>
+                      Data Science
+                    </Text>
+                    <Text size="small" className={styles.menuDescription}>
+                      Data Science is an interdisciplinary field which utilizes
+                      scientific methods, algorithms, and data insights to
+                      extract meaning and drive informed decision-making across
+                      industries.
+                    </Text>
+                  </div>
+
+                  <div className={styles.menuFooter}>
+                    <div className={styles.cards}>
+                      <Text>Courses</Text>
+                      <MenuCourseCard />
+                      <MenuCourseCard />
+                      <MenuCourseCard />
+                      <MenuCourseCard />
                     </div>
 
-                    <div className={styles.menuFooter}>
-                      <div>
-                        <Text>Courses</Text>
+                    <div className={styles.cards}>
+                      <Text>Certificate</Text>
 
-                        <div className={styles.cards}>
-                          <div className={styles.cardOne}>
-                            <Text>Healthcare Analytics Bootcamp</Text>
-
-                            <div className={styles.details}>
-                              <Text size="caption">4-6 Months</Text>
-                              <Text size="caption" className={styles.students}>
-                                <UserFilled size={12} /> 563 Students
-                              </Text>
-                            </div>
-                          </div>
-                          <div className={styles.cardOne}>
-                            <Text>Healthcare Analytics Bootcamp</Text>
-
-                            <div className={styles.details}>
-                              <Text size="caption">4-6 Months</Text>
-                              <Text size="caption" className={styles.students}>
-                                <UserFilled size={12} /> 563 Students
-                              </Text>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div>
-                        <Text>Certificate</Text>
-
-                        <div className={styles.cards}>
-                          <MenuCertificateCard />
-                          <MenuCertificateCard />
-                          <MenuCertificateCard />
-                          <MenuCertificateCard />
-                        </div>
-                      </div>
+                      <MenuCertificateCard />
+                      <MenuCertificateCard />
+                      <MenuCertificateCard />
+                      <MenuCertificateCard />
                     </div>
                   </div>
-                )}
-              </div>
+                </div>
+              )}
             </nav>
           )}
         </div>
