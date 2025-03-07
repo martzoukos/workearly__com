eebc0fa3f879086391ed9cc4362fea100f4c8a91
Ronak Baldha <5601641+ron4ex@users.noreply.github.com>
2025-03-07 12:53:00 +0530

    Fix minor issues

--- apps/web/components/_pages/DefaultPage/DefaultPage.tsx
+++ apps/web/components/_pages/DefaultPage/DefaultPage.tsx
@@ -3,6 +3,7 @@ import PageItem from "@/components/PageItem";
 import usePageResolver from "@/hooks/usePageResolver";
 import { useContentful } from "@/stores/ContentfulStore";
 import clsx from "clsx";
+import { useRouter } from "next/router";
 import styles from "./DefaultPage.module.scss";
 
 type PropsType = {
@@ -10,15 +11,20 @@ type PropsType = {
 };
 
 export default function DefaultPage({ className }: PropsType) {
+  const router = useRouter();
   const { page } = useContentful();
   const { preDividerItems, postDividerItems } = usePageResolver(page);
 
+  const isHome = router.asPath === "/";
+
   return (
-    <main className={clsx(styles.root, className)}>
-      <Breadcrumbs
-        className={styles.breadcrumbs}
-        items={[{ name: "Home", href: "/" }, { name: page.name || "" }]}
-      />
+    <main className={clsx(styles.root, styles.isHome, className)}>
+      {!isHome && (
+        <Breadcrumbs
+          className={styles.breadcrumbs}
+          items={[{ name: "Home", href: "/" }, { name: page.name || "" }]}
+        />
+      )}
       {[...preDividerItems, ...postDividerItems].map((item) => (
         <PageItem key={item?.sys.id} item={item} />
       ))}
