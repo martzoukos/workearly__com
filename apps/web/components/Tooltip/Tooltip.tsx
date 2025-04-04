4b4d7b843abef977895ea69fedba1ca1eaa7ab37
Hamza Wattari <hamza.ayment@gmail.com>
2025-04-04 19:54:33 +0300

    Fixing changes

--- apps/web/components/Tooltip/Tooltip.tsx
+++ apps/web/components/Tooltip/Tooltip.tsx
@@ -1,27 +1,50 @@
 import { Information } from "@carbon/icons-react";
-import { Tooltip as RadixTooltip } from "radix-ui";
+import { Popover, Tooltip as RadixTooltip } from "radix-ui";
 import styles from "./Tooltip.module.scss";
 import clsx from "clsx";
+import Viewport from "@/components/Viewport";
 
 export default function Tooltip({
   children,
   className,
 }: RadixTooltip.TooltipContentProps) {
   return (
-    <RadixTooltip.Provider>
-      <RadixTooltip.Root>
-        <RadixTooltip.Trigger asChild>
-          <Information />
-        </RadixTooltip.Trigger>
-        <RadixTooltip.Portal>
-          <RadixTooltip.Content
+    <>
+      <RadixTooltip.Provider delayDuration={0}>
+        <RadixTooltip.Root>
+          <Viewport showAfter="md">
+            <RadixTooltip.Trigger asChild>
+              <Information />
+            </RadixTooltip.Trigger>
+          </Viewport>
+
+          <RadixTooltip.Portal>
+            <RadixTooltip.Content
+              className={clsx(styles.content, className)}
+              sideOffset={5}
+            >
+              {children} <RadixTooltip.Arrow className={styles.arrow} />
+            </RadixTooltip.Content>
+          </RadixTooltip.Portal>
+        </RadixTooltip.Root>
+      </RadixTooltip.Provider>
+      <Popover.Root>
+        <Viewport showUntil="md">
+          <Popover.Trigger asChild>
+            <Information />
+          </Popover.Trigger>
+        </Viewport>
+
+        <Popover.Portal>
+          <Popover.Content
             className={clsx(styles.content, className)}
             sideOffset={5}
           >
-            {children} <RadixTooltip.Arrow className={styles.arrow} />
-          </RadixTooltip.Content>
-        </RadixTooltip.Portal>
-      </RadixTooltip.Root>
-    </RadixTooltip.Provider>
+            {children}
+            <Popover.Arrow className={styles.arrow} />
+          </Popover.Content>
+        </Popover.Portal>
+      </Popover.Root>
+    </>
   );
 }
