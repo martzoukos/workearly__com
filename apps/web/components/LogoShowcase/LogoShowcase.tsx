c4488c889f23b0cbf84771373ee8f6c246c267d7
Spiros Martzoukos <s.d.martzoukos@gmail.com>
2025-08-09 13:49:02 +0300

    Show logos and fix mobile behaviour.

--- apps/web/components/LogoShowcase/LogoShowcase.tsx
+++ apps/web/components/LogoShowcase/LogoShowcase.tsx
@@ -2,6 +2,9 @@ import { QueryItem } from "@workearly/api";
 import clsx from "clsx";
 import Image from "next/image";
 import styles from "./LogoShowcase.module.scss";
+import Viewport from "@/components/Viewport";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
 
 type PropsType = {
   assets: QueryItem["Asset"][];
@@ -18,19 +21,63 @@ export default function LogoShowcase({
     "--column-count": columnCount,
   } as React.CSSProperties;
 
+  // Group assets into chunks of 4 for 2x2 grid layout
+  const groupAssetsIntoSlides = (
+    assets: QueryItem["Asset"][],
+    groupSize: number,
+  ) => {
+    const groups = [];
+    for (let i = 0; i < assets.length; i += groupSize) {
+      groups.push(assets.slice(i, i + groupSize));
+    }
+    return groups;
+  };
+
+  const assetGroups = groupAssetsIntoSlides(assets, 4);
+
   return (
-    <div className={clsx(styles.root, className)} style={style}>
-      {assets.map((asset) => (
-        <div key={asset.sys.id} className={styles.wrapper}>
-          <Image
-            src={asset.url || ""}
-            alt={asset.description || ""}
-            width={140}
-            height={70}
-            className={styles.image}
-          />
+    <>
+      <Viewport showUntil="md">
+        <Swiper
+          spaceBetween={16}
+          slidesPerView={1}
+          loop={true}
+          modules={[Autoplay]}
+          // autoplay={{ delay: 3000, disableOnInteraction: false }}
+          className={styles.slider}
+        >
+          {assetGroups.map((group, index) => (
+            <SwiperSlide key={`group-${index}`}>
+              <div className={styles.gridSlide}>
+                {group.map((asset) => (
+                  <ImageWrapper key={asset.sys.id} asset={asset} />
+                ))}
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Viewport>
+      <Viewport showAfter="md">
+        <div className={clsx(styles.root, className)} style={style}>
+          {assets.map((asset) => (
+            <ImageWrapper key={asset.sys.id} asset={asset} />
+          ))}
         </div>
-      ))}
-    </div>
+      </Viewport>
+    </>
   );
+
+  function ImageWrapper({ asset }: { asset: QueryItem["Asset"] }) {
+    return (
+      <div className={styles.wrapper}>
+        <Image
+          src={asset.url || ""}
+          alt={asset.description || ""}
+          width={140}
+          height={70}
+          className={styles.image}
+        />
+      </div>
+    );
+  }
 }
